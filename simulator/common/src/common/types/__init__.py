2e79ab1173557e35305f049e4340ed251861fe2d
Eugene <evgenijgoncarevic@gmail.com>
2026-02-17 16:54:58 +0300

    Coords type removed

--- simulator/common/src/common/types/__init__.py
+++ simulator/common/src/common/types/__init__.py
@@ -3,11 +3,6 @@ from typing import Required, TypedDict
 from sqlalchemy import UUID
 
 
-class CoordsType(TypedDict):
-    lon: float
-    lat: float
-
-
 class SensorBufferType(TypedDict):
     """
     Standardized dictionary for sensor measurement buffering.
